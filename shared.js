1b171a5310255b8e2be2060bc013d695ba1de6a3
maryuri <maryurip.ramirez@ucuenca.edu.ec>
2021-11-09 20:46:58 -0500

    Cambios9

--- shared.js
+++ shared.js
@@ -5,38 +5,24 @@ var selectPlanButtons = document.querySelectorAll(".plan button");
 var toggleButton = document.querySelector('.toggle-button');
 var mobileNav = document.querySelector('.mobile-nav');
 
-console.dir(backdrop.style['background-image']);
 
-//console.dir(backdrop);
 for(var i = 0; i < selectPlanButtons.length; i++){
     selectPlanButtons[i].addEventListener("click", function() {
-       modal.classList.add('open');
-       backdrop.classList.add('open');
+       modal.style.display = "block";
+       backdrop.style.display = "block";
     });
 }
 
-backdrop.addEventListener("click", function() {
-    // mobileNav.style.display  = 'none';
-   mobileNav.classList.remove("open");
-   closeModal();
-});
+backdrop.addEventListener("click", closeModal);
 
-if (modalNoButton){
- modalNoButton.addEventListener("click", closeModal);  
-}
+modalNoButton.addEventListener("click", closeModal);
 
 function closeModal() {
-//   backdrop.style.display = "none";
-//   modal.style.display = "none";
- if (modal) {
-    modal.classList.remove('open');
- }
-     backdrop.classList.remove('open');
+   backdrop.style.display = "none";
+   modal.style.display = "none";
 }
 
-toggleButton.addEventListener('click', function() {
-   // mobileNav.style.display = 'block';
-   // backdrop.style.display = 'block';
-   mobileNav.classList.add('open');
-   backdrop.classList.add('open');
+toggleButton.addEventListener("click", function() {
+   mobileNav.style.display = "block";
+   backdrop.style.display = "block";
 });
